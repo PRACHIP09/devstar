3726ab652cc8e2dbab76c3ea71a05535062628c5
BhojrajCSE21 <bhojrajnarware@gmail.com>
2023-10-30 07:12:57 +0530

    qr scanner

--- src/routes/(tools)/qr-scanner/+page.svelte
+++ src/routes/(tools)/qr-scanner/+page.svelte
@@ -1,17 +1,65 @@
-<script lang="ts">
-	import Intro from '$lib/Intro.svelte';
-	import { Label, Input, Range, Radio, Checkbox } from 'flowbite-svelte';
-	import { onMount } from 'svelte';
+<script>
+    import { Html5Qrcode } from 'html5-qrcode'
+    import { onMount } from 'svelte'
 
-	export let data;
-	
+    let scanning = false
+
+    let html5Qrcode
+
+    onMount(init)
+
+    function init() {
+        html5Qrcode = new Html5Qrcode('reader')
+    }
+
+    function start() {
+        html5Qrcode.start(
+            { facingMode: 'environment' },
+            {
+                fps: 10,
+                qrbox: { width: 250, height: 250 },
+            },
+            onScanSuccess,
+            onScanFailure
+        )
+        scanning = true
+    }
+
+    async function stop() {
+        await html5Qrcode.stop()
+        scanning = false
+    }
+
+    function onScanSuccess(decodedText, decodedResult) {
+        alert(`Code matched = ${decodedText}`)
+        console.log(decodedResult)
+    }
+
+    function onScanFailure(error) {
+        console.warn(`Code scan error = ${error}`)
+    }
 </script>
 
-<Intro heading={data.meta.title} description={data.meta.description} />
+<style>
+    main {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        gap: 20px;
+    }
+    reader {
+        width: 100%;
+        min-height: 500px;
+        background-color: black;
+    }
+</style>
 
-<svelte:window bind:innerWidth bind:innerHeight />
-
-<section class="*">
-	<div class="*">
-	</div>
-</section>
+<main>
+    <reader id="reader"/>
+    {#if scanning}
+        <button on:click={stop}>stop</button>
+    {:else}
+        <button on:click={start}>start</button>
+    {/if}
+</main>
